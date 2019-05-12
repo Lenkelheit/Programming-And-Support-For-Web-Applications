9717f939f15aee1a833f0c6aba1364841e1e018a
Nazariy <tymtsiv2710@gmail.com>
2019-05-12 17:43:08 +0300

    Add database context and its initializer

---
+++ Task_2/PizzaShop/PizzaShop/Data/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaShop.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+using PizzaShop.Models;
+
+namespace PizzaShop.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<PizzaIngredient> PizzaIngredients { get; set; }
+        public DbSet<Reviews> Reviews { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PizzaIngredient>().HasKey(pin => new { pin.PizzaId, pin.IngredientId });
+
+            modelBuilder.Entity<PizzaIngredient>()
+                .HasOne(pin => pin.Pizza)
+                .WithMany(pi => pi.PizzaIngredients)
+                .HasForeignKey(pin => pin.PizzaId);
+
+            modelBuilder.Entity<PizzaIngredient>()
+                .HasOne(pin => pin.Ingredient)
+                .WithMany(ing => ing.PizzaIngredients)
+                .HasForeignKey(pin => pin.IngredientId);
+
+            // adds data
+            //dbInitializer.Seed(modelBuilder);
+        }
+    }
+}
--- Task_2/PizzaShop/PizzaShop/Startup.cs
+++ Task_2/PizzaShop/PizzaShop/Startup.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OnlinePizzaWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaShop.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using PizzaShop.Models.Entities;
 
 namespace PizzaShop
 {
@@ -23,23 +25,64 @@ namespace PizzaShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<AppDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sp => ShoppingCart.GetCart(sp));
+
+            services.AddMvc();
+
+            services.AddMemoryCache();
+            services.AddSession();
+
+            services.Configure<IdentityOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                // Password settings
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
             });
 
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/LogOut";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -47,7 +90,8 @@ namespace PizzaShop
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseSession();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
@@ -55,6 +99,7 @@ namespace PizzaShop
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
