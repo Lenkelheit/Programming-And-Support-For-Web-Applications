38072bc30b00eb75eb88c76f3648f9f111703ad0
Nazariy <tymtsiv2710@gmail.com>
2019-05-15 02:36:10 +0300

    Add components

---
+++ Task_2/PizzaShop/PizzaShop/Components/CarouselMenu.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaShop.Data;
+using PizzaShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShop.Components
+{
+    public class CarouselMenu : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public CarouselMenu(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pizzas = await _context.Pizzas.Where(x => x.IsPizzaOfTheWeek).ToListAsync();
+            return View(pizzas);
+        }
+    }
+}
